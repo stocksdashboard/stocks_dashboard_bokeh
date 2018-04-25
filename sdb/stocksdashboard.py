59a1b849b4fbb97bc567d87b3fd6a7bf95ea1537
Mabel Villalba <mabelvj@gmail.com>
2018-04-25 18:28:20 +0200

    Revert "[WIP] Fixes #5 Changed df by data in Formatter, added self.names to Formatter, removed method check_variables from Formatter."
    This reverts commit 822e9f5f78b2fb7d14c2df4fe704df06b11d6220.

--- sdb/stocksdashboard.py
+++ sdb/stocksdashboard.py
@@ -205,77 +205,90 @@ class StocksDashboard():
 class Formatter():
 
     def __init__(self):
-        self.name = None
+        pass
 
     def format_data(self, data):
         return self.check_datasource(data)
 
+    def check_variables(self, varname=None):
+
+        def _is_valid_type(__varname=None, __value=None):
+            if not __value:
+                msg = "'%s cannot be None.'" % __varname
+                raise(ValueError(msg))
+            elif not isinstance(__value, int):
+                msg = "'%s cannot be of" % __varname + \
+                      "type %s, must be 'int'" % type(__value)
+                raise (TypeError(msg))
+            else:
+                return True
+
+        if varname and hasattr(self, varname):
+            return _is_valid_type(varname, getattr(self, varname))
+        else:
+            for _varname, _value in self.__dict__.items():
+                if _varname in ['width', 'height', 'ncols']:
+                    _is_valid_type(_varname, _value)
+
     def retrieve_names(self, data):
-        """
-            Retrieve names if the attribute 'names'
-            has the same length as data.
-        """
-        if self.names and len(self.names) == len(data):
+        if self.names and len(self.names[-1]) == len(data):
             names = self.names[-1]
         else:
             names = np.arange(len(data))
         return names
 
-    def check_valid(self, data):
+    def check_valid(self, df):
         """
             Check that the dataframes for plotting are the valid type.
         """
-        def __is_valid_type(data):
-            __valid_types = (pd.DataFrame, pd.Series, list, dict, np.ndarray)
+        if not (df is None or isinstance(df,
+                                         (pd.DataFrame, pd.Series,
+                                          list, dict, np.ndarray))):
 
-            if not (data is None or isinstance(data, __valid_types)):
+            raise(ValueError("Inapropiate value of df: %s." % df +
+                             "Expected pandas.DataFrame, pandas.Series, " +
+                             "or list of pandas objects"""))
 
-                raise(ValueError("Inapropiate value of df: %s." % data +
-                                 "Expected pandas.DataFrame, pandas.Series, " +
-                                 "or list of pandas objects"""))
-        # Check data is valid type
-        __is_valid_type(data)
-
-        if isinstance(data, list):
-            if all([isinstance(d, dict) for d in data]):
+        if isinstance(df, list):
+            if all([isinstance(d, dict) for d in df]):
                 return [pd.DataFrame.from_dict(d)
                         if not isinstance(d.values(), dict)
                         else StocksDashboard.check_valid(d)
-                        for d in data]
+                        for d in df]
             else:
-                return StocksDashboard.check_valid(data[0])
-        if isinstance(data, dict):
-            if all([isinstance(d, dict) for k, d in data.items()
+                return StocksDashboard.check_valid(df[0])
+        if isinstance(df, dict):
+            if all([isinstance(d, dict) for k, d in df.items()
                     if hasattr(d, 'values')]):
-                result = {k: pd.DataFrame.from_dict(d) for k, d in data.items()
+                result = {k: pd.DataFrame.from_dict(d) for k, d in df.items()
                           if hasattr(d, 'values')}
                 self.names = list(result.keys())
                 return list(result.values())
-            elif all([isinstance(d, pd.DataFrame) for k, d in data.items()
+            elif all([isinstance(d, pd.DataFrame) for k, d in df.items()
                       if hasattr(d, 'values')]):
-                self.names = list(data.keys())
-                return list(data.values())
+                self.names = list(df.keys())
+                return list(df.values())
             else:
                 raise(ValueError("Dict containing objects" +
-                                 " of tpye %s." % type(data) +
+                                 " of tpye %s." % type(df) +
                                  "Please convert to format" +
                                  " \{'name': \{'date': ..., 'values': ...\}\}"
                                  "or {'name': {'date': pd.DataFrame}."))
         return True
 
-    def check_datasource(self, data, inplace=False):
-        # Check data is pandas or list of pandas
+    def check_datasource(self, df, inplace=False):
+        # Check df is pandas or list of pandas
 
-        result = self.check_valid(data)
+        result = self.check_valid(df)
 
         if not isinstance(result, bool):
-            data = result
+            df = result
 
-        if not isinstance(data, (list, dict)):
+        if not isinstance(df, (list, dict)):
             # Convert to a list of, at least,
             # one element, to be able to iterate.
-            if not isinstance(data, pd.DataFrame):
-                data = [pd.DataFrame(data)]
+            if not isinstance(df, pd.DataFrame):
+                df = [pd.DataFrame(df)]
             else:
-                data = [data]
-        return copy.deepcopy(data), self.names
+                df = [df]
+        return copy.deepcopy(df), self.names
